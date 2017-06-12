9ea35b99faca42abf58210406b02696924fb8e7f
[travisbailey945] <[travisbailey945@gmail.com]>
2017-06-11 19:44:43 -0500

    Just the framework

---
+++ challenge_6_3.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+'''
+	Challenge 6.3 - Winning Division
+	Travis Bailey
+	Sun Jun 11, 2017
+
+	"Write a program that determines which of a company's four divisions, (Northeast, Southeast, Northwest, and Southwest) had the greatest sales for a quarter."
+
+'''
+
+import logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+logging.debug(Start of Program)
